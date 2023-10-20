de1c59976910052bb279395c1b9a6f1df1ef0355
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-10-19 20:29:08 -0600

    Estructura de layout

---
+++ INTEGRADOR/resources/views/aboutus.blade.php
@@ -0,0 +1,18 @@
+@extends ('layouts.mainlayout')
+@section ('content')
+<img src="" alt="">
+
+<section>
+    <h1>¿Quienes somos?</h1>
+    <p></p>
+    <img src="" alt="">
+</section>
+
+<section>
+    <h1>Objetivo</h1>
+    <p></p>
+    <img src="" alt="">
+</section>
+
+
+@stop
