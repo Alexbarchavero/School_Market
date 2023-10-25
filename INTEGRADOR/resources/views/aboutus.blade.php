7ba639354a01ea0369fbdd0290de5b1939961b6d
Edwin13142 <115176421+Edwin13142@users.noreply.github.com>
2023-10-24 20:29:27 -0600

    Actualizacion del about us

--- INTEGRADOR/resources/views/aboutus.blade.php
+++ INTEGRADOR/resources/views/aboutus.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.mainlayout')
 @section('content')
-<img src="" alt="">
+<img src="#" alt="palosespeciales">
 
 <section>
     <h1>¿Quienes somos?</h1>
-    <p></p>
-    <img src="" alt="">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consectetur dignissimos reprehenderit sit sunt expedita cum explicabo nam, dolores a quos ut esse neque minus? Quae, sunt aut? Quisquam, excepturi?</p>
+    <img src="#" alt="palosespeciales">
 </section>
 
 <section>
     <h1>Objetivo</h1>
-    <p></p>
-    <img src="" alt="">
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet recusandae totam ipsa labore, hic voluptatem vitae, sunt nihil in consectetur dolorum. Nobis ea ex laudantium molestias quam, consectetur minus fugiat!</p>
+    <img src="#" alt="palosespeciales">
 </section>
 
 
