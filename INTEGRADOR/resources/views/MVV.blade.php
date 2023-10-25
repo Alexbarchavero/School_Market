2b198c513fe6c6cf198f719251571328f7ace230
OscarMS6550 <125920933+OscarMS6550@users.noreply.github.com>
2023-10-24 20:20:30 -0600

    MVVDI

--- INTEGRADOR/resources/views/MVV.blade.php
+++ INTEGRADOR/resources/views/MVV.blade.php
@@ -2,19 +2,28 @@
 @section('content')
 <img src="" alt="">
 
+<style>
+    body {
+            text-align: center;
+        }
+        h1 {
+            color:green;
+        }
+</style>
+
 
 <section>
     <h1>VISION</h1>
     <p>Nuestra visión es ser líderes en la industria, proporcionando productos/servicios de alta 
         calidad que superen las expectativas de nuestros clientes.</p>
-    <img src="" alt="">
+        <img src="https://th.bing.com/th/id/OIG.L1qU56KDoW9zXmaOqetz?pid=ImgGn&w=1024&h=1024&rs=1" alt="Vision" width="200">
 </section>
 
 <section>
     <h1>MISION</h1>
     <p>Nuestra misión es proporcionar soluciones innovadoras y confiables a nuestros clientes, 
         contribuyendo al éxito de sus negocios y creando un entorno de trabajo excepcional para nuestros empleados.</p>
-    <img src="" alt="">
+    <img src="https://th.bing.com/th/id/OIG.Bhd2.ycMYlH8h5wZXTLT?pid=ImgGn&w=1024&h=1024&rs=1" alt="Mision" width="200">
 </section>
 
 
@@ -26,7 +35,9 @@
         <li>Innovación:Fomentamos la creatividad y la mejora continua.</li>
         <li>Compromiso:Estamos comprometidos con la satisfacción de nuestros clientes y el éxito de nuestros empleados.</li>
     </ul>
-    <img src="" alt="">
+    <img src="https://th.bing.com/th/id/OIG.zS2AA2iW9Bor1Fo7M31K?pid=ImgGn" alt="Valores" width="200">
 </section>
 
 
+
+@stop
