9d05168cc00cc71fc5fea772b0d15f32911c1687
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-10-26 20:19:59 -0600

    Todo

--- INTEGRADOR/resources/views/practica.blade.php
+++ INTEGRADOR/resources/views/practica.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.mainlayout')
 @section('content')
 
-<div style="width: 50%;">
-    <img src="https://www.kantekpremium.mx/wp-content/uploads/2023/03/perro_chihuahua.jpg" alt="imagen de chihuahua" style="width: 50em;">
+<div>
+    <img id="imgch" src="https://www.kantekpremium.mx/wp-content/uploads/2023/03/perro_chihuahua.jpg" alt="imagen de chihuahua">
 </div>
 
 <div>
