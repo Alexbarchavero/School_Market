f96a80bedc655eddd9d8e496673a1f8a0d02cf3c
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-11-09 15:24:19 -0600

    Cambios aboutus

--- INTEGRADOR/resources/views/aboutus.blade.php
+++ INTEGRADOR/resources/views/aboutus.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.mainlayout')
 @section('content')
-<img src="#" alt="palosespeciales">
 
-<section>
+<section id="seccion-1">
     <h1>¿Quienes somos?</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consectetur dignissimos reprehenderit sit sunt expedita cum explicabo nam, dolores a quos ut esse neque minus? Quae, sunt aut? Quisquam, excepturi?</p>
-    <img src="#" alt="palosespeciales">
+    <div class="pimg">
+        <p>
+            Somos un equipo comprometido que busca promover la sostenibilidad y reducir la huella ecológica en el ámbito escolar. Nuestro proyecto integrador se enfoca en el desarrollo de una plataforma que facilita la compra, venta e intercambio de productos escolares de segunda mano. Nos esforzamos por fomentar la reutilización de estos productos, contribuyendo así a la preservación del medio ambiente y brindando una solución económica y ecológica para la comunidad escolar.
+        </p>
+        <img src="https://img.freepik.com/foto-gratis/equipo-gente-negocios-apilando-manos_53876-119699.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1699056000&semt=ais" alt="imagen de quienes somos">
+    </div>
 </section>
 
-<section>
+<section id="seccion-2">
     <h1>Objetivo</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet recusandae totam ipsa labore, hic voluptatem vitae, sunt nihil in consectetur dolorum. Nobis ea ex laudantium molestias quam, consectetur minus fugiat!</p>
-    <img src="#" alt="palosespeciales">
+    <div class="imgp">
+        <img src="https://definicion.de/wp-content/uploads/2019/04/objetivo-general.png" alt="imagen de objetivo">
+        <p>
+            El objetivo principal de nuestro proyecto es crear una plataforma eficiente y segura que promueva la reutilización de productos escolares de segunda mano, reduciendo así la huella ecológica en el ámbito educativo y proporcionando una solución económica y sostenible para estudiantes y familias.
+        </p>
+    </div>
 </section>
-
 
 @stop
--- INTEGRADOR/resources/views/layouts/mainlayout.blade.php
+++ INTEGRADOR/resources/views/layouts/mainlayout.blade.php
@@ -6,22 +6,26 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/styleC.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}"/>
     <link rel="stylesheet" type="text/css" href="{{asset('css/vmv.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/aboutus.css')}}">
     <title>SCHOOLMARKET</title>
-
 </head>
-<header id="main-header">
-    <a href="#" class="logo">
-        <img src="{{ asset('public/img/LogoP.png') }}" alt="Logo" class="logo-img" style="width: 120px; height: auto;">
-    </a>
-    <nav class="navigation">
-        <a href="#">Acerca de</a>
-        <a href="#">Productos</a>
-        <a href="#">Contacto</a>
-        <button class="bthLogin-popup">Iniciar sesión</button>
-    </nav>
-</header>
-    
+
+<body>
+        
+    <header id="main-header">
+        <a href="#" class="logo">
+            <img src="{{ asset('public/img/LogoP.png') }}" alt="Logo" class="logo-img" style="width: 120px; height: auto;">
+        </a>
+        <nav class="navigation">
+            <a href="#">Acerca de</a>
+            <a href="#">Productos</a>
+            <a href="#">Contacto</a>
+            <button class="bthLogin-popup">Iniciar sesión</button>
+        </nav>
+    </header>
+        
     @yield('content')
+    
     <footer>
         <p>©Derechos Reservados</p>
     </footer>
